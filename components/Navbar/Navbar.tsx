a5f2e44f04ea2a0ba160a2c36d583296abc33b80
Jack Matthews <jackhkmatthews@gmail.com>
2021-12-19 21:57:01 +0000

    feat(Navbar): add active states

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,26 +1,42 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <ul className={styles.root}>
-      <li className={styles.item}>
-        <Link href="/">
-          <a className={styles.link}>Home</a>
-        </Link>
-      </li>
-      <li className={styles.item}>
+      <li
+        className={[
+          styles.item,
+          styles.dad,
+          router.pathname === "/dad" ? styles.isActive : "",
+        ].join(" ")}
+      >
         <Link href="/dad">
           <a className={styles.link}>Dad</a>
         </Link>
       </li>
-      <li className={styles.item}>
-        <Link href="/mum">
-          <a className={styles.link}>Mum</a>
+      <li
+        className={[
+          styles.item,
+          styles.mama,
+          router.pathname === "/mama" ? styles.isActive : "",
+        ].join(" ")}
+      >
+        <Link href="/mama">
+          <a className={styles.link}>Mama</a>
         </Link>
       </li>
-      <li className={styles.item}>
+      <li
+        className={[
+          styles.item,
+          styles.holly,
+          router.pathname === "/holly" ? styles.isActive : "",
+        ].join(" ")}
+      >
         <Link href="/holly">
           <a className={styles.link}>Holly</a>
         </Link>
